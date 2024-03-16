39bf3958afe1d425edb4adeeee7d2bb370c1683d
andersonhavah <havahanderson@gmail.com>
2024-03-16 12:08:27 +0000

    Finished Week 02 Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -9,7 +9,7 @@ class Program
         job1._jobTitle = "Software Engineer";
         job1._company = "Microsoft";
         job1._startYear = 2019;
-        job1._endYear = 2022;
+        job1._endYear = 2022; 
 
         // Create the second instance of job class
         Job job2 = new Job();
