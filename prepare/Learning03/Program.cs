f7ecc596d9cd016c1cbb247d2f61c01e7ab7ae72
andersonhavah <havahanderson@gmail.com>
2024-03-27 10:15:26 +0000

    Finished Week 03 Learning Activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,18 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Create an instance of the Fraction class using the contructor with no parameters
         Fraction fraction1 = new Fraction();
         Console.WriteLine($"{fraction1.GetFractionString()}");
         Console.WriteLine($"{fraction1.GetDecimalValue()}");
 
+        // Create an instance of the Fraction class using the contructor with one parameter
         Fraction fraction2 = new Fraction(5);
         Console.WriteLine($"{fraction2.GetFractionString()}");
         Console.WriteLine($"{fraction2.GetDecimalValue()}");
 
+        // Create an instance of the Fraction class using the contructor with two parameters
         Fraction fraction3 = new Fraction(3, 4);
         Console.WriteLine($"{fraction3.GetFractionString()}");
         Console.WriteLine($"{fraction3.GetDecimalValue()}");
 
+        // Create an instance of the Fraction class using the contructor with two parameters
         Fraction fraction4 = new Fraction(1, 3);
         Console.WriteLine($"{fraction4.GetFractionString()}");
         Console.WriteLine($"{fraction4.GetDecimalValue()}");
