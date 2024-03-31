da4b6de4965badc293a24d10d12024b9fca0673e
andersonhavah <havahanderson@gmail.com>
2024-03-31 09:34:26 +0000

    Word Class File for Week 03 Develop Project

---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,39 @@
+// Class representing a single word in the scripture
+class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+    public void Show()
+    {
+        _isHidden = false;
+    }
+
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+
+    public string GetDisplayText()
+    {
+        if (_isHidden)
+        {
+            return new string('_', _text.Length);
+        }
+        else
+        {
+            return _text;
+        }
+    }
+}
