e70be9542334c980a2a1a0dcc5d03e10ffe8fab0
andersonhavah <havahanderson@gmail.com>
2024-04-07 21:15:55 +0000

    Develop Week 04 completed

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Collections.Generic;
+using System.Threading;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        // Sample usage
+        Console.WriteLine("Welcome to the Mindfulness Program!");
+        while (true)
+        {
+            Console.WriteLine("Choose an activity:");
+            Console.WriteLine("1. Start Breathing Activity");
+            Console.WriteLine("2. Start Reflection Activity");
+            Console.WriteLine("3. Start Listing Activity");
+            Console.WriteLine("4. Quit");
+            Console.Write("Select a choice from the menu: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            // Activity activity;
+            switch (choice)
+            {
+                case 1:
+                    BreathingActivity breathingActivity = new BreathingActivity();
+                    breathingActivity.Run();
+                    break;
+                case 2:
+                    ReflectingActivity reflectingActivity = new ReflectingActivity();
+                    reflectingActivity.Run();
+                    break;
+                case 3:
+                    ListingActivity listingActivity = new ListingActivity();
+                    listingActivity.Run();
+                    break;
+                case 4:
+                    Console.WriteLine("Bye! See you next time!");
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice. Exiting program.");
+                    return;
+            }
+        }
+
     }
 }
