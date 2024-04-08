e38a1e5b7bbaee6e26f183d0a1cd71c5c2e5acc3
andersonhavah <havahanderson@gmail.com>
2024-04-08 01:24:27 +0000

    BreathingActivity Class completed - Week 04

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -4,32 +4,27 @@ public class BreathingActivity : Activity
     // This is the constructor of the class
     public BreathingActivity()
     {
-        SetName("Welcome to the Breathing Activity.\n");
-        SetDescription("This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing. \n ");
+        SetName("Welcome to the Breathing Activity.");
+        SetDescription("This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.");
     }
 
     public void Run()
     {
         Console.Clear();
         DisplayStartingMessage();
-        // Console.Write("How long, in seconds, would you like for your sessions?");
-        // _duration = int.Parse(Console.ReadLine());
         ShowSpinner(3);
 
         // Implement breathing in/out logic
-        for (int i = 2; i < GetDuration(); i++)
+        for (int i = 0; i * 2 < GetDuration(); i++)
         {
-            Console.Write("Breathe in...");
+            Console.WriteLine("Breathe in...");
             ShowCountDown(i);
-            Thread.Sleep(1000);
-            Console.Write("\b \b");
-            Console.Write("Now breathe out...");
+            Console.WriteLine("Now breathe out...");
             ShowCountDown(i);
-            Thread.Sleep(1000);
-            Console.Write("\b \b");
-            Console.WriteLine();
+            Console.WriteLine("");
         }
 
+        ShowSpinner(3);
         DisplayEndingMessage();
     }
 }
